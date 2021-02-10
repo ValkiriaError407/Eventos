2012df29cb59f125a5045490c1bc277d3a35d245
“Valkiria” <valkiriavieira@gmail.com>
2021-02-10 00:32:04 -0300

    Commit eventos

---
+++ segundapaginaeventos.js
@@ -0,0 +1,18 @@
+
+window.addEventListener("load",incio);
+
+function incio(){
+        document.getElementById("blusas").addEventListener("click", blusasF);
+        document.getElementById("vestidos").addEventListener("click", vestidosF);
+        document.getElementById("pantalones").addEventListener("click", pantalonesF);
+
+	}
+function blusasF () {
+    alert("Stock Disponible!");
+}
+function vestidosF () {
+    alert("Stock Disponible!");
+}
+function pantalonesF () {
+    alert("Stock Disponible!");
+}
